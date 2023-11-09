64411cee952ff3dd85c2e0aab80afc2af2f1bde8
yanniedog <jkokavec@gmail.com>
2023-11-09 22:18:41 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,27 @@
 #!/bin/bash
 
-# Function to display error message and exit
-function display_error {
-    echo "Error: $1"
-    exit 1
-}
+# Check if Python3 and pip are installed, and if not, exit with an error message
+command -v python3 &>/dev/null || { echo "Error: Python3 is required but not found. Please install it."; exit 1; }
+command -v pip &>/dev/null || { echo "Error: Pip is required but not found. Please install it."; exit 1; }
 
-# Check if Python3 and pip are installed
-if ! command -v python3 &>/dev/null || ! command -v pip &>/dev/null; then
-    display_error "Python3 and pip are required but not found. Please install them."
-fi
+# Set the directory path
+directory="/home/pi/blueberry"
 
-# Create the blueberry directory if it doesn't exist
-if [ ! -d "/home/pi/blueberry" ]; then
-    mkdir /home/pi/blueberry || display_error "Failed to create the 'blueberry' directory."
-fi
+# Create the directory if it doesn't exist
+mkdir -p "$directory" || { echo "Error: Failed to create the 'blueberry' directory."; exit 1; }
 
 # Change to the blueberry directory
-cd /home/pi/blueberry || display_error "Failed to change directory to '/home/pi/blueberry'."
+cd "$directory" || { echo "Error: Failed to change directory to '$directory'."; exit 1; }
 
 # Clone the GitHub repository
-if ! git clone https://github.com/yanniedog/blueberry.git .; then
-    display_error "Failed to clone the repository."
-fi
+git clone https://github.com/yanniedog/blueberry.git . || { echo "Error: Failed to clone the repository."; exit 1; }
 
-# Create a virtual environment
-python3 -m venv venv || display_error "Failed to create the virtual environment."
-
-# Activate the virtual environment
-source venv/bin/activate || display_error "Failed to activate the virtual environment."
+# Create and activate a virtual environment
+python3 -m venv venv || { echo "Error: Failed to create the virtual environment."; exit 1; }
+source venv/bin/activate || { echo "Error: Failed to activate the virtual environment."; exit 1; }
 
 # Install required Python packages
-pip install -r requirements.txt || display_error "Failed to install Python packages."
+pip install -r requirements.txt || { echo "Error: Failed to install Python packages."; exit 1; }
 
 # Deactivate the virtual environment
 deactivate
