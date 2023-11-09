37dec99aad319775ca9b42678a6a362bf035953c
yanniedog <jkokavec@gmail.com>
2023-11-09 22:29:09 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@
 command -v python3 &>/dev/null || { echo "Error: Python3 is required but not found. Please install it."; exit 1; }
 command -v pip &>/dev/null || { echo "Error: Pip is required but not found. Please install it."; exit 1; }
 
-# Set the directory path
-directory="/home/pi/blueberry"
+# Define the user's home directory dynamically
+HOME_DIR=$(eval echo ~$USER)
+
+# Set the directory path within the user's home directory
+directory="$HOME_DIR/blueberry"
 
 # Create the directory if it doesn't exist
 mkdir -p "$directory" || { echo "Error: Failed to create the 'blueberry' directory."; exit 1; }
@@ -22,6 +25,12 @@ source venv/bin/activate || { echo "Error: Failed to activate the virtual enviro
 
 # Install required Python packages
 pip install -r requirements.txt || { echo "Error: Failed to install Python packages."; exit 1; }
+
+# Make the blueberry.py script executable
+chmod +x blueberry.py || { echo "Error: Failed to make the 'blueberry.py' script executable."; exit 1; }
+
+# Create a symbolic link in /usr/local/bin
+ln -sf "$directory/blueberry.py" /usr/local/bin/blueberry || { echo "Error: Failed to create a symbolic link."; exit 1; }
 
 # Deactivate the virtual environment
 deactivate
