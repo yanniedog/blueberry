2a8d9ae4cd96de5aac65810b26563fe7ac25d498
yanniedog <jkokavec@gmail.com>
2023-11-15 16:34:55 +1100

    Added unique_id to output rows

--- blueberry.py
+++ blueberry.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 import time
 from colorama import Fore, Style, init
 import statistics
+import uuid
 
 config = configparser.ConfigParser()
 config_file_path = 'config.me'
@@ -35,7 +36,6 @@ with open(os.path.expanduser(OUI_FILE_PATH), 'r') as file:
             company_name = parts[1].strip()
             oui_data[mac_prefix] = company_name
             
-# load the unique ID generated during setup.sh
 def load_environment():
     home_dir = os.path.expanduser('~')  # This gets the home directory
     env_file = os.path.join(home_dir, 'blueberry', 'env.dat')  # Construct the path to env.dat
@@ -44,8 +44,14 @@ def load_environment():
         for line in file:
             if 'unique_id' in line:
                 return line.split('=')[1].strip().strip("'")
+    
+    # Generate a new unique_id and save it to env.dat
+    new_unique_id = str(uuid.uuid4())
+    with open(env_file, 'a') as file:
+        file.write(f"unique_id = '{new_unique_id}'\n")
+    
+    return new_unique_id
 
-unique_id = load_environment()
 
 def color_rssi(value, colorize=True):
     if not value:
@@ -130,7 +136,7 @@ def get_oui_info(mac_address):
         print(f"API request failed: {e}")
         return None
 
-def update_csv(found_devices):
+def update_csv(found_devices, unique_id):
     if not os.path.exists(os.path.expanduser(CSV_FILE_PATH)):
         create_csv_file()
 
@@ -168,7 +174,8 @@ def update_csv(found_devices):
                 'Manufacturer': manufacturer_info,
                 'rssi_list': ','.join(map(str, rssi_list)),
                 'Name': device['Name'] if device['Name'] else existing_row.get('Name', ''),
-                'Seen Count': int(existing_row.get('Seen Count', '0')) + 1
+                'Seen Count': int(existing_row.get('Seen Count', '0')) + 1,
+                'unique_id': unique_id  # Add unique_id to the row
             })
             updated_rows.append(existing_row)
         else:
@@ -187,7 +194,8 @@ def update_csv(found_devices):
                 'Manufacturer': manufacturer_info,
                 'rssi_list': device['RSSI'],
                 'Name': device['Name'],
-                'Seen Count': 1
+                'Seen Count': 1,
+                'unique_id': unique_id  # Add unique_id to the row
             }
             updated_rows.append(new_row)
 
@@ -196,9 +204,10 @@ def update_csv(found_devices):
 
 def create_csv_file():
     with open(os.path.expanduser(CSV_FILE_PATH), 'w', newline='') as csvfile:
-        fieldnames = ['Last Seen', 'First Seen', 'MAC', 'Name', 'RSSI', 'Min RSSI', 'Mean RSSI', 'Max RSSI', 'sd', 'Dur', 'Manufacturer', 'rssi_list', 'Seen Count']
+        fieldnames = ['Last Seen', 'First Seen', 'MAC', 'Name', 'RSSI', 'Min RSSI', 'Mean RSSI', 'Max RSSI', 'sd', 'Dur', 'unique_id', 'Manufacturer', 'rssi_list', 'Seen Count']  # Add 'unique_id' between 'Dur' and 'Manufacturer'
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
+
 
 def read_csv_file():
     if not os.path.exists(os.path.expanduser(CSV_FILE_PATH)):
@@ -209,13 +218,13 @@ def read_csv_file():
 
 def write_csv_file(rows):
     with open(os.path.expanduser(CSV_FILE_PATH), 'w', newline='') as csvfile:
-        fieldnames = ['Last Seen', 'First Seen', 'MAC', 'Name', 'RSSI', 'Min RSSI', 'Mean RSSI', 'Max RSSI', 'sd', 'Dur', 'Manufacturer', 'rssi_list', 'Seen Count']
+        fieldnames = ['Last Seen', 'First Seen', 'MAC', 'Name', 'RSSI', 'Min RSSI', 'Mean RSSI', 'Max RSSI', 'sd', 'Dur', 'Manufacturer', 'rssi_list', 'Seen Count', 'unique_id']  # Add 'unique_id' to fieldnames
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for row in sorted(rows, key=lambda x: int(float(x['RSSI'])), reverse=True):
             writer.writerow(row)
 
-def process_btmgmt_output():
+def process_btmgmt_output(unique_id):
     global total_loops, oui_cache
     api_call_made = False
     print("Starting process_btmgmt_output")
@@ -251,9 +260,8 @@ def process_btmgmt_output():
     finally:
         process.stdout.close()
         process.wait()
-    update_csv(found_devices)
+    update_csv(found_devices, unique_id)  # Pass unique_id to update_csv
     print("Ending process_btmgmt_output")
-
 
 
 def read_and_display_csv():
@@ -261,30 +269,31 @@ def read_and_display_csv():
     if os.path.exists(os.path.expanduser(CSV_FILE_PATH)) and os.path.getsize(os.path.expanduser(CSV_FILE_PATH)) > 0:
         with open(os.path.expanduser(CSV_FILE_PATH), mode='r', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
-            print(f"{'Last Seen':<20} {'First Seen':<20} {'MAC':<18} {'% seen':<8} {'RSSI':<7} {'Min':<7} {'Mean':<7} {'Max':<7} {'sd':<7} {'Dur':<10} {'Manufacturer':<30}")
+            print(f"{'Last Seen':<20} {'First Seen':<20} {'MAC':<18} {'% seen':<8} {'RSSI':<7} {'Min':<7} {'Mean':<7} {'Max':<7} {'sd':<7} {'Dur':<10} {'unique_id':<36} {'Manufacturer':<30}")  # Add 'unique_id' between 'Dur' and 'Manufacturer' in the header
 
             rows = [row for row in reader]
             rows.sort(key=lambda x: x.get(SORT_KEY, ""), reverse=(SORT_ORDER == 'descending'))
 
-            if rows:  # Check if rows is not empty
+            if rows:  # Check if rows are not empty
                 for row in rows:
                     last_seen = row.get('Last Seen', '').ljust(20)
                     first_seen = row.get('First Seen', '').ljust(20)
-                    mac = row.get('MAC', '').ljust(18) # determines gap between MAC and % seen (18 is a good number)
+                    mac = row.get('MAC', '').ljust(18)  # determines gap between MAC and % seen (18 is a good number)
                     name = row.get('Name', '').ljust(0)
                     rssi = row.get('RSSI', '0')
                     min_rssi = row.get('Min RSSI', '0')
                     mean_rssi = row.get('Mean RSSI', '0')
                     max_rssi = row.get('Max RSSI', '0')
-                    sd = row.get('sd', '0').ljust(7) # determines the gap between sd and Dur
-                    dur = row.get('Dur', '00:00:00').ljust(10) 
-                    manufacturer = row.get('Manufacturer', '').ljust(0) # determines the gap between RSSI and Min. 0 is a good number
+                    sd = row.get('sd', '0').ljust(7)  # determines the gap between sd and Dur
+                    dur = row.get('Dur', '00:00:00').ljust(10)
+                    unique_id = row.get('unique_id', '').ljust(10)  # Get 'unique_id' from the row and adjust padding
+                    manufacturer = row.get('Manufacturer', '').ljust(0)  # determines the gap between Dur and Manufacturer. 0 is a good number
                     seen_count = row.get('Seen Count', '0')
 
                     colorize = (datetime.now() - datetime.strptime(last_seen.strip(), '%Y-%m-%d %H:%M:%S')).total_seconds() < 10
 
-                    rssi_padded = color_rssi(rssi, colorize).ljust(16)  # determines the gap between RSSI and Min. 10 is a good number
-                    min_rssi_padded = color_rssi(min_rssi, colorize).ljust(16) # determines the gap between Min and Avg RSSI. 13 is a good number
+                    rssi_padded = color_rssi(rssi, colorize).ljust(16)  # determines the gap between RSSI and Min. 16 is a good number
+                    min_rssi_padded = color_rssi(min_rssi, colorize).ljust(16)  # determines the gap between Min and Avg RSSI. 16 is a good number
                     mean_rssi_padded = color_rssi(mean_rssi, colorize).ljust(16)
                     max_rssi_padded = color_rssi(max_rssi, colorize).ljust(16)
                     sd_padded = (Fore.LIGHTBLACK_EX if not colorize else '') + sd.ljust(7) + Style.RESET_ALL
@@ -292,23 +301,25 @@ def read_and_display_csv():
                     manufacturer_padded = (Fore.LIGHTBLACK_EX if not colorize else '') + manufacturer.ljust(0) + Style.RESET_ALL
 
                     loops = max(1, total_loops)
-                    seen_percentage = (Fore.LIGHTBLACK_EX if not colorize else '') + f"{int(row.get('Seen Count', '0')) / loops * 100:.2f}%".ljust(8) + Style.RESET_ALL # determines the gap between % Seen (Name) and RSSI. 7 is a good number
+                    seen_percentage = (Fore.LIGHTBLACK_EX if not colorize else '') + f"{int(row.get('Seen Count', '0')) / loops * 100:.2f}%".ljust(8) + Style.RESET_ALL  # determines the gap between % Seen (Name) and RSSI. 8 is a good number
 
                     # Concatenate Name with Manufacturer
                     manufacturer_info = row.get('Manufacturer', '')
                     if row.get('Name'):
                         manufacturer_info += f" ({row.get('Name')})"
 
-                    print(f"{last_seen} {first_seen} {mac} {seen_percentage} {rssi_padded} {min_rssi_padded} {mean_rssi_padded} {max_rssi_padded} {sd} {dur} {manufacturer_info.ljust(30)}")
+                    print(f"{last_seen} {first_seen} {mac} {seen_percentage} {rssi_padded} {min_rssi_padded} {mean_rssi_padded} {max_rssi_padded} {sd} {dur} {unique_id} {manufacturer_info.ljust(30)}")  # Add 'unique_id' between 'Dur' and 'Manufacturer' in the row output
             else:
                 print("No data found in the CSV file.")
     else:
         print("CSV file does not exist or is empty.")
 
+
 if __name__ == "__main__":
+    unique_id = load_environment()  # Get the unique_id
     while True:
         print("Starting main loop iteration")
-        process_btmgmt_output()
+        process_btmgmt_output(unique_id)  # Pass unique_id to process_btmgmt_output
 
         total_loops += 1  # Increment total_loops here
         print(f"Debug - Total Loops: {total_loops}")
